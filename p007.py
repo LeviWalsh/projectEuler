47e538e24aa6106ac82f0b0eb3d03ce967ff5de1
Levi Walsh <leviwalsh@berkeley.edu>
2017-06-04 22:24:08 -0400

    Problem 7

---
+++ p007.py
@@ -0,0 +1,18 @@
+# What is the 10 001st prime number?
+
+def isPrime(n):
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primes = [2]
+n = 3
+while len(primes) < 10001:
+	if isPrime(n):
+		primes.append(n)
+	n += 2
+
+print(primes[-1])
+
+# 104743
