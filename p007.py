aac17bf122bd0bf3c7c60c1d63c44fa385bdca7d
Levi Walsh <leviwalsh@berkeley.edu>
2017-06-19 00:42:27 -0400

    changed up problem 7

--- p007.py
+++ p007.py
@@ -1,6 +1,7 @@
 # What is the 10 001st prime number?
 
 def isPrime(n):
+	assert n > 1: "Primes are always greater than 1"
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
